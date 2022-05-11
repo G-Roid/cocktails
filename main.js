6cdc77c26b616eecaa75c4253c502aa6b0c8564b
Geoffroy Guillotte <geoffroy.guillotte@gmail.com>
2022-05-11 12:30:49 +1000

    added basic fetch request

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('hello juice')
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => {
+    console.log(`error: ${err}`)
+})
