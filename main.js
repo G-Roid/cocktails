95b78e7999050bfdbe38354977e0a2b3104ab967
Geoffroy Guillotte <geoffroy.guillotte@gmail.com>
2022-05-11 12:54:36 +1000

    added eventlistener onto search btn

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(err => {
-    console.log(`error: ${err}`)
-})
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink() {
+    let choice = document.querySelector('input').value;
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => {
+        console.log(`error: ${err}`)
+    })
+}
+
